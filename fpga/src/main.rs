68a9925e9972ea0aaca1e6e069bf4d0464d70440
codeblooded1729 <kapilpause007@gmail.com>
2023-10-03 14:23:40 +0530

    first commit

---
+++ fpga/src/main.rs
@@ -0,0 +1,20 @@
+use fpga::{SendBuffer, F1, Fpga};
+
+fn encode(a: u64, b: u64, twiddle: u64) -> SendBuffer {
+    let mut buffer = SendBuffer::default();
+    buffer[..8].copy_from_slice(&a.to_le_bytes());
+    buffer[8..16].copy_from_slice(&b.to_le_bytes());
+    buffer[16..24].copy_from_slice(&twiddle.to_le_bytes());
+    buffer
+}
+
+fn main(){
+    let mut fpga = F1::new(0, 0).unwrap();
+    println!("init");
+
+    let index = 0;
+    let send_buffer = encode(456, 123, 789);
+    fpga.send(index, &send_buffer);
+    println!("wrote");
+    println!("{}", fpga);
+}
